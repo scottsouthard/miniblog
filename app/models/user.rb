f772b7d4804fb60f7d5f28335b30f7d3bfecbc04
Scott Southard <scottmsouthard@gmail.com>
2016-06-30 12:21:01 -0500

    Setup models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  has_many :posts
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  root to: 'posts#show'
+
+  get 'sessions/new' => "sessions#new"
+  post 'sessions/create' => "sessions#create"
+  get 'sessions/logout' => "sessions#logout"
+
 end
